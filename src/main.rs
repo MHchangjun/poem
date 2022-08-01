d29a9e1f121b2674849cc3f98a85ad5777c2eb20
song <opopgta@gmail.com>
2022-08-02 01:20:03 +0900

    feat : routing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod poem;
+
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(poem::today_subject)
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
