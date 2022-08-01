95f8ddf3e56b2bb52c9113e919e667d9ac8e0606
song <opopgta@gmail.com>
2022-08-02 01:36:58 +0900

    feat : set subject

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(subject::today_subject)
+            .service(subject::set)
     })
         .bind("127.0.0.1:8080")?
         .run()
--- src/subject.rs
+++ src/subject.rs
@@ -1,7 +1,13 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, post, HttpResponse, Responder};
 
 #[get("/subject")]
 pub async fn today_subject() -> impl Responder {
     HttpResponse::Ok()
         .body("삼행시")
+}
+
+#[post("/subject")]
+pub async fn set(subject: String) -> impl Responder {
+    HttpResponse::Ok()
+        .body(subject)
 }
