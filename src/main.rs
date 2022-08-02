0aa44d04569c48609d3ff0d3d126d907a8daca4b
song <opopgta@gmail.com>
2022-08-02 18:30:39 +0900

    feat : create db pool

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
 mod subject;
 
-use actix_web::{App, HttpServer};
+#[macro_use]
+extern crate diesel;
+
+use std::env;
+use actix_web::{web, App, HttpServer};
+use diesel::r2d2::ConnectionManager;
+use diesel::{r2d2, SqliteConnection};
+
+mod models;
+mod schema;
+
+type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv::dotenv().ok();
+
+    let conn_spec = env::var("DATABASE_URL").expect("DATABASE_URL");
+    let manager = ConnectionManager::<SqliteConnection>::new(conn_spec);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool");
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool.clone()))
             .service(subject::get)
             .service(subject::set)
     })
