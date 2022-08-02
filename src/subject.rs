cb61ed62613aef30f201b7e1b450c1b243eab7ef
song <opopgta@gmail.com>
2022-08-02 18:28:33 +0900

    feat : set subject

--- src/subject.rs
+++ src/subject.rs
@@ -1,4 +1,11 @@
-use actix_web::{get, post, HttpResponse, Responder};
+use diesel::prelude::*;
+use actix_web::{web, get, post, HttpResponse, Responder};
+
+use crate::DbPool;
+
+use crate::models;
+
+type DbError = Box<dyn std::error::Error + Send + Sync>;
 
 #[get("/subject")]
 pub async fn get() -> impl Responder {
@@ -6,8 +13,39 @@ pub async fn get() -> impl Responder {
         .body("삼행시")
 }
 
+pub fn insert_subject(
+    _subject: &str,
+    conn: &SqliteConnection
+) -> Result<models::Subject, DbError> {
+    use crate::schema::subjects::dsl::*;
+
+    let new_subject = models::Subject {
+        id: "test".to_owned(),
+        subject: _subject.to_owned(),
+    };
+
+    diesel::insert_into(subjects)
+        .values(&new_subject)
+        .execute(conn)?;
+
+    Ok(new_subject)
+}
+
 #[post("/subject")]
-pub async fn set(subject: String) -> impl Responder {
-    HttpResponse::Ok()
-        .body(subject)
+pub async fn set(pool: web::Data<DbPool>, subject: String) -> impl Responder {
+    let subject = web::block(move || {
+        let conn = pool.get()?;
+        insert_subject(&subject, &conn)
+    })
+        .await;
+
+    match subject {
+        Ok(_) => {
+            HttpResponse::Ok()
+                // .body(subject)
+        }
+        Err(_) => {
+            HttpResponse::NoContent()
+        }
+    }
 }
